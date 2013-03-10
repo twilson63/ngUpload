4d732f10210c6ee49eb7d045263ad1202301534b
Tom Wilson <tom@jackhq.com>
2013-03-09 23:16:29 -0500

    added spec and refactored out $ dependency

--- examples/nodejs/libs/js/ng-upload.js
+++ examples/nodejs/libs/js/ng-upload.js
@@ -21,96 +21,91 @@
 //  });
 //
 angular.module('ngUpload', [])
-  .directive('ngUpload', function () {
-      return {
-          restrict: 'A',
-          link: function (scope, element, attrs) {
+  .directive('ngUpload', ['$parse', function ($parse) {
+    return {
+      restrict: 'A',
+      link: function (scope, element, attrs) {
 
-              // Options (just 1 for now) 
-              // Each option should be prefixed with 'upload-Options-' or 'uploadOptions'
-              // {
-              //    // specify whether to enable the submit button when uploading forms
-              //    enableControls: bool 
-              // }
-              var options = {};
-              options.enableControls = attrs['uploadOptionsEnableControls'];
-              
-              // get scope function to execute on successful form upload
-              if (attrs['ngUpload']) {
+        // Options (just 1 for now) 
+        // Each option should be prefixed with 'upload-Options-' or 'uploadOptions'
+        // {
+        //    // specify whether to enable the submit button when uploading forms
+        //    enableControls: bool 
+        // }
+        var options = {};
+        options.enableControls = attrs.uploadOptionsEnableControls;
+        
+        // get scope function to execute on successful form upload
+        if (attrs.ngUpload) {
 
-                  element.attr("target", "upload_iframe");
-                  element.attr("method", "post");
+          element.attr("target", "upload_iframe");
+          element.attr("method", "post");
 
-                  // Append a timestamp field to the url to prevent browser caching results
-                  element.attr("action", element.attr("action") + "?_t=" + new Date().getTime());
+          // Append a timestamp field to the url to prevent browser caching results
+          element.attr("action", element.attr("action") + "?_t=" + new Date().getTime());
 
-                  element.attr("enctype", "multipart/form-data");
-                  element.attr("encoding", "multipart/form-data");
+          element.attr("enctype", "multipart/form-data");
+          element.attr("encoding", "multipart/form-data");
 
-                  // Retrieve the callback function
-                  var fn = attrs['ngUpload'].split('(')[0];
-                  var callbackFn = scope.$eval(fn);
-                  if (callbackFn == null || callbackFn == undefined || !angular.isFunction(callbackFn))
-                  {
-                      var message = "The expression on the ngUpload directive does not point to a valid function.";
-                      // console.error(message);
-                      throw message + "\n";
-                  }                      
+          // Retrieve the callback function
+          var fn = $parse(attrs.ngUpload);
+          
+          if (!angular.isFunction(fn)) {
+              var message = "The expression on the ngUpload directive does not point to a valid function.";
+              throw message + "\n";
+          }                      
 
-                  // Helper function to create new iframe for each form submission
-                  var addNewDisposableIframe = function (submitControl) {
-                      // create a new iframe
-                      var iframe = $("<iframe id='upload_iframe' name='upload_iframe' border='0' width='0' height='0' style='width: 0px; height: 0px; border: none; display: none' />");
+          // Helper function to create new iframe for each form submission
+          var addNewDisposableIframe = function(submitControl) {
+              // create a new iframe
+              var iframe = angular.element("<iframe id='upload_iframe' name='upload_iframe' border='0' width='0' height='0' style='width: 0px; height: 0px; border: none; display: none' />");
 
-                      // attach function to load event of the iframe
-                      iframe.bind('load', function () {
+              // attach function to load event of the iframe
+              iframe.bind('load', function () {
 
-                              // get content - requires jQuery
-                              var content = iframe.contents().find('body').text();
+                // get content - requires jQuery
+                var content = iframe.contents().find('body').text();
+                // execute the upload response function in the active scope
+                scope.$apply(function () { 
+                  fn(scope, { content: content, completed: content !== ""});
+                });
+                // remove iframe
+                if (content !== "") // Fixes a bug in Google Chrome that dispose the iframe before content is ready.
+                    setTimeout(function () { iframe.remove(); }, 250);
 
-                              // execute the upload response function in the active scope
-                              scope.$apply(function () { callbackFn(content, content !== "" /* upload completed */); });
+                //if (options.enableControls == null || !(options.enableControls.length >= 0))
+                submitControl.attr('disabled', null);
+                submitControl.attr('title', 'Click to start upload.');
+              });
 
-                              // remove iframe
-                              if (content != "") // Fixes a bug in Google Chrome that dispose the iframe before content is ready.
-                                  setTimeout(function () { iframe.remove(); }, 250);
+            // add the new iframe to application
+            element.parent().append(iframe);
+          };
 
-                              //if (options.enableControls == null || !(options.enableControls.length >= 0))
-                              submitControl.attr('disabled', null);
-                              submitControl.attr('title', 'Click to start upload.');
-                          });
+          // 1) get the upload submit control(s) on the form (submitters must be decorated with the 'ng-upload-submit' class)
+          // 2) attach a handler to the controls' click event
+          var submitControl = element.find('.upload-submit');
+          submitControl.bind('click', function () {
 
-                      // add the new iframe to application
-                      element.parent().append(iframe);
-                  };
+            addNewDisposableIframe(submitControl /* pass the submit control */);
 
-                  // 1) get the upload submit control(s) on the form (submitters must be decorated with the 'ng-upload-submit' class)
-                  // 2) attach a handler to the controls' click event
-                  $('.upload-submit', element).click(
-                      function () {
+            scope.$apply(function () { fn(scope, {content: "Please wait...", completed: false } /* upload not completed */); });
 
-                          addNewDisposableIframe($(this) /* pass the submit control */);
+            var enabled = true;
+            if (options.enableControls === null || options.enableControls === undefined || options.enableControls.length >= 0) {
+                // disable the submit control on click
+                submitControl.attr('disabled', 'disabled');
+                enabled = false;
+            }
 
-                          scope.$apply(function () { callbackFn("Please wait...", false /* upload not completed */); });
+            submitControl.attr('title', (enabled ? '[ENABLED]: ' : '[DISABLED]: ') + 'Uploading, please wait...');
 
-                          //console.log(angular.toJson(options));
-
-                          var enabled = true;
-                          if (options.enableControls === null || options.enableControls === undefined || options.enableControls.length >= 0) {
-                              // disable the submit control on click
-                              $(this).attr('disabled', 'disabled');
-                              enabled = false;
-                          }
-
-                          $(this).attr('title', (enabled ? '[ENABLED]: ' : '[DISABLED]: ') + 'Uploading, please wait...');
-
-                          // submit the form
-                          $(element).submit();
-                      }
-                  ).attr('title', 'Click to start upload.');
-              }
-              else
-                  console.log("No callback function found on the ngUpload directive.");
-          }
-      };
-  });
+            // submit the form
+            element.submit();
+          }).attr('title', 'Click to start upload.');
+        }
+        else
+            console.log("No callback function found on the ngUpload directive.");
+    }
+};
+}]);
--- test/ngUploadSpec.js
+++ test/ngUploadSpec.js
@@ -1,1 +1,28 @@
 // TODO
+describe('ngUpload', function() {
+  var elm, scope;
+  beforeEach(module('ngUpload'));
+  beforeEach(inject(function($rootScope, $compile) {
+    elm = angular.element(
+      '<div>' +
+        '<form ng-upload="foo()">' +
+          '<input type="file" name="foo"></input>' +
+          '<input class="upload_submit" type="submit" value="submit"></input>' +
+        '</form>' +
+      '</div>');
+    scope = $rootScope;
+    $compile(elm)(scope);
+    //scope.digest();
+  }));
+  it('should set form upload attributes', function() {
+    var form = elm.find('form');
+    expect(form).toBeDefined();
+    expect(form.attr('enctype')).toBe('multipart/form-data');
+    expect(form.attr('encoding')).toBe('multipart/form-data');
+    expect(form.attr('target')).toBe('upload_iframe');
+  });
+  it('should find submitcontrol', function() {
+    var submitControl = elm.find('form .upload_submit');
+    expect(submitControl).toBeDefined();
+  });
+})
