61042adecd31bdc975c264de1a5060d536a8cf5e
mguymon <michael@tobedevoured.com>
2013-07-12 11:33:10 -0400

    add option to enable rails csrf, copy ng-model values to input values on submit

--- ng-upload.js
+++ ng-upload.js
@@ -70,6 +70,7 @@ angular.module('ngUpload', [])
             var content = iframe.contents().find('body').html();
             try {
               content = $.parseJSON(iframe.contents().find('body').text());
+              if (console) { console.log(content); }
             } catch (e) {
               if (console) { console.log('WARN: XHR response is not valid json'); }
             }
@@ -98,6 +99,10 @@ angular.module('ngUpload', [])
           // why do we need this???
           element.attr('title', (enabled ? '[ENABLED]: ' : '[DISABLED]: ') + 'Uploading, please wait...');
 
+          form.find('*[ng-model]').each( function() {
+            $(this).attr('value', scope.$eval( $(this).attr('ng-model') ));
+          });
+          
           form.submit();
 
         }).attr('title', 'Click to start upload.');
@@ -108,6 +113,17 @@ angular.module('ngUpload', [])
     return {
       restrict: 'AC',
       link: function (scope, element, attrs) {
+        
+        // Options (just 1 for now) 
+        // Each option should be prefixed with 'upload-options-' or 'uploadOptions'
+        // {
+        //    // add the Rails CSRF hidden input to form
+        //    enableRailsCsrf: bool 
+        // }
+        
+        var options = {};
+        options.enableRailsCsrf = attrs.uploadOptionsEnableRailsCsrf;
+        
         element.attr("target", "upload_iframe");
         element.attr("method", "post");
         // Append a timestamp field to the url to prevent browser caching results
@@ -115,6 +131,17 @@ angular.module('ngUpload', [])
         element.attr("action", element.attr("action") + separator + "_t=" + new Date().getTime());
         element.attr("enctype", "multipart/form-data");
         element.attr("encoding", "multipart/form-data");
+
+        // If enabled, add csrf hidden input to form
+        if ( options.enableRailsCsrf ) {
+            $("<input />")
+              .attr("id", "upload-csrf-token")
+              .attr("type", "hidden")
+              .attr("name", $('meta[name=csrf-param]').attr('content') )
+              .val( $('meta[name=csrf-token]').attr('content') )
+              .appendTo(element);
+         
+        }
       }
     };
   }]);
---
+++ test/ngUploadRailsCsrfSpec.js
@@ -0,0 +1,33 @@
+// ngUpload as directive and input submit
+describe('ngUpload', function() {
+  var elm, scope;
+  beforeEach(module('ngUpload'));
+  beforeEach(inject(function($rootScope, $compile) {
+    elm = angular.element(
+      '<div>' +
+        '<form action="/upload" ng-upload upload-options-enable-rails-csrf="true">' +
+          '<input type="file" name="foo"></input>' +
+          '<input type="submit" value="submit" upload-submit="foo()"></input>' +
+        '</form>' +
+      '</div>');
+    scope = $rootScope;
+    $compile(elm)(scope);
+    //scope.digest();
+    
+  }));
+  it('should set csrf hidden field', function() {
+    var form = elm.find('form');
+    expect(form).toBeDefined();
+    
+    var submit = elm.find('input[type="submit"]');
+    submit.click();
+    
+    var iframe = elm.find('#upload_iframe');
+    expect(iframe[0]).toBeDefined();    
+    expect(elm.find('#upload-csrf-token')[0]).toBeDefined();
+    
+    //expect(elm.find('#upload-csrf-token').val()).toBe('test_token');
+    
+
+  });
+})
