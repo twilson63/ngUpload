f9f3565c82bd8fb3a5e78680bc2f80091a62c09f
Nathan Kot <me@nathankot.com>
2014-01-02 16:41:38 +1300

    A little code cleanup.

--- test/ngUploadHrefSubmitSpec.js
+++ test/ngUploadHrefSubmitSpec.js
@@ -14,6 +14,7 @@ describe('ngUpload', function() {
     $compile(elm)(scope);
     //scope.digest();
   }));
+
   it('should set form upload attributes', function() {
     var form = elm.find('form');
     expect(form).toBeDefined();
