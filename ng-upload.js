078282b043776438c497eb3b828b27417ce1cb00
Nathan Kot <me@nathankot.com>
2014-01-02 17:06:01 +1300

    options.beforeSubmit needs to have scope passed in.

--- ng-upload.js
+++ ng-upload.js
@@ -146,7 +146,7 @@ angular.module('ngUpload', [])
         // Start upload
         element.bind('submit', function uploadStart() {
           // perform check before submit file
-          if (options.beforeSubmit) { return options.beforeSubmit(); }
+          if (options.beforeSubmit) { return options.beforeSubmit(scope); }
           // If convertHidden option is enabled, set the value of hidden fields to the eval of the ng-model
           if (options.convertHidden) {
             angular.forEach(element.find('input'), function(el) {
